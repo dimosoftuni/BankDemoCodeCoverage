7811b5085250fb2abb8c5af02cc0fa70dcfc5a05
Dimo Mitev <dimosoftuni@gmail.com>
2025-01-26 10:16:10 +0200

    update the amount

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -43,7 +43,7 @@ namespace Bank
 
         public void Withdraw(decimal amount)
         {
-            if (amount < 1000)
+            if (amount < 100)
             {
                 amount += (amount * 0.05m);
                 this.Amount -= amount;
